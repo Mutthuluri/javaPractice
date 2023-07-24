3eac1be3a65b125ef3a05b40f0f9013fee2823c6
Mutthuluri Varun kumar <139310468+Mutthuluri@users.noreply.github.com>
2023-07-24 13:49:40 +0530

    Add files via upload

---
+++ kumarGirlFriends.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.Scanner;
+
+public class kumarGirlFriends {
+
+	public static void main(String[] args) {
+		Scanner vv = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int number = vv.nextInt();
+     switch(number) {
+     case 1: {
+    	 System.out.println("adiLakshmi proposed me");
+     }
+     break;
+     case 2: {
+    	 System.out.println("I Proposed Janu");
+     }
+     break;
+     case 3: {
+    	 System.out.println("Honey proposed me in parul sem 3");
+     }
+     break;
+     default:{
+    	 System.out.println("He don't stop searching for girls");
+     }
+     }
+	}
+
+}
