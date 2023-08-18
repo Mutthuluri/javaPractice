0c69be04ff1a21e2bddb57c2a728bf703e358545
Mutthuluri Varun kumar <139310468+Mutthuluri@users.noreply.github.com>
2023-08-17 19:27:39 -0700

    Create WAJP to read the data from the user and calculate the sum of two numbers.java

---
+++ WAJP to read the data from the user and calculate the sum of two numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner vv = new Scanner(System.in);
+    int  number1 = vv.nextInt();
+    int number2 = vv.nextInt();
+    int number3 = number1+number2;
+    System.out.println(number3);
+  }
+}
